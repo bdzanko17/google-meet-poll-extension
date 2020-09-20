4209245c54e2e0c52d8863ad4cd2ade41289b6b9
bdzanko17 <bdzanko1@gmail.com>
2020-09-20 19:05:36 +0200

    small changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,56 @@
 (function () {
-
-  var googleMeetCall=0;
+  var googleMeetCall = 0;
   var questionActive;
-   var room=window.location.href;
+  var room;
 
-  var y =setInterval(() => {
-    if(document.querySelector('#ow3 > div.T4LgNb > div > div:nth-child(5) > div.crqnQb > div.pHsCke > div.Jrb8ue > div')!=null)
-    googleMeetCall=1;
+  var y = setInterval(() => {
+    if (
+      document.querySelector(
+        "#ow3 > div.T4LgNb > div > div:nth-child(5) > div.crqnQb > div.rG0ybd.LCXT6 > div.f0WtFf > div.M5zXed > div > span > span > span"
+      ) != null
+    ) {
+      googleMeetCall = 1;
+      room = window.location.href;
+      var x = 0;
+      for (var i = 0; i < room.length; i++) {
+        if (room.charAt(i) === "?") 
+          break;
+        
+        x++;
+      }
+      console.log(x);
+
+      if (room.includes("?")) {
+        console.log("sadrzi");
+        room = room.slice(0, x);
+        console.log(room);
+      }
+    }
   }, 1000);
 
-  //Prikaz samo kada je u call-u, provjera da li je u callu, ako jeste prikazi dugme :) 
-  var x =setInterval(() => {
- 
-    if(googleMeetCall){
- 
-      //OTVARAM SOCKET ZA REAL TIME PROMJENE
-      var socket = io.connect("https://poll-serverr.herokuapp.com/");
-      socket.emit('join', room);
+  //Prikaz samo kada je u call-u, provjera da li je u callu, ako jeste prikazi dugme :)
+  var x = setInterval(() => {
+    if (googleMeetCall) {
+      clearInterval(y);
 
-      var organizator=0;
+      //OTVARAM SOCKET ZA REAL TIME PROMJENE
+      var socket = io.connect("http://localhost:3000/");
+      socket.emit("join", room);
+
+      var organizator = 0;
       socket.on("active_poll", (data) => {
-        questionActive=data;
+        questionActive = data;
         //OSLUŠKIVANJE DA LI IMA AKTIVNE ANKETE, AKO DODJE PORUKA AKTIVNA ANKETA POJAVI SE ONO DA SE ODGOVARA
         $(".odgovor").addClass("active");
         $(".question").html("Question: " + data);
       });
 
-      socket.on("organizator",()=>{
+      socket.on("organizator", () => {
         console.log("organizator");
-            organizator=1;
-            alert("You are now organizatorr");
+        organizator = 1;
+        alert("You are now organizatorr");
       });
-    
+
       socket.on("results", (data) => {
         $("#question").text(questionActive);
         $("#ans1").text(data[1]);
@@ -39,26 +58,21 @@
         $("#ans3").text(data[3]);
         $(".rezultati").addClass("active");
       });
-    
+
       var x = document.createElement("P"); // Create a <p> node
       x.className = "benga"; //Dodavanje klase na element radi selekcuje u css-u
-    
-     
-    //var t = document.createTextNode("Ask a question!");
-    var button = document.createElement("BUTTON"); // Create a text node
-    button.className = "open";
-    button.innerHTML = "ASK A QUESTION";
-    //x.appendChild(t);
-    // Append the text to <p>
-    x.appendChild(button);
-    document.body.appendChild(x); // Append <p> to <body>
-    
-    
-    
-    
-    
-    
-      $(".benga").append(`<div class='popup-overlay'> <div class='popup-content'>
+
+      //var t = document.createTextNode("Ask a question!");
+      var button = document.createElement("BUTTON"); // Create a text node
+      button.className = "open";
+      button.innerHTML = "ASK A QUESTION";
+      //x.appendChild(t);
+      // Append the text to <p>
+      x.appendChild(button);
+      document.body.appendChild(x); // Append <p> to <body>
+
+      $(".benga")
+        .append(`<div class='popup-overlay'> <div class='popup-content'>
         <div class="chat-popup" id="myForm">
           <textarea class="question" placeholder="Type your question.." name="msg" required></textarea>
           <textarea class="code" placeholder="Code" name="msg" required></textarea>
@@ -67,7 +81,7 @@
           <button class="closebtn">Close</button>
       </div> 
        </div></div>`);
-    
+
       $(".benga").append(`
       <div class='odgovor'>
     
@@ -116,62 +130,52 @@
 
       `);
 
-    
       $(".answers").on("change", function () {
         $(".answers").not(this).prop("checked", false);
       });
-    
+
       $(".open").on("click", function () {
-        
         $(".popup-overlay, .popup-content").addClass("active");
       });
-    
+
       //removes the "active" class to .popup and .popup-content when the "Close" button is clicked
       $(".closebtn").on("click", function () {
         $(".popup-overlay, .popup-content").removeClass("active");
       });
-    
+
       //SLANJE POLL-a
       $(".sendbtn").on("click", function () {
         var data = $(".question").val(); //UZIMANJE PITANJA IZ TEXTBOXA I SLANJE SERVERU DA IMA AKTIVNA ANKETA, NAKON TOGA SERVER ODGOVARA SVIMA DA IMA AKTIVNA ANKETA
         var code = $(".code").val();
 
-        if(organizator)
-        socket.emit("vote", {room,data,code});
+        if (organizator) socket.emit("vote", { room, data, code });
         else alert("You are not an organizer");
         $(".popup-overlay, .popup-content").removeClass("active");
-    
       });
 
       $(".sendbtncode").on("click", function () {
         var codee = $(".code").val();
-        socket.emit("code",codee);
-          });
-    
+        socket.emit("code", codee);
+      });
+
       //SLANJE ODGOVORA
       $(".answer").on("click", function () {
         $('input[name="ans"]:checked').each(function () {
-          var answer=this.value;
-          socket.emit("votes", answer); //SLANJE ODGOVORA NA SERVER, SVAKO MOZE SAMO JEDNOM POSLAT ODGOVOR
+          var answer = this.value;
+          socket.emit("votes", { answer, room }); //SLANJE ODGOVORA NA SERVER, SVAKO MOZE SAMO JEDNOM POSLAT ODGOVOR
         });
         $(".odgovor").removeClass("active"); //SKRIVANJE ODGOVORA
       });
 
-
       $("#CloseRes").on("click", function () {
         $(".rezultati").removeClass("active");
       });
-
-
     }
-    
   }, 1000);
 
   setInterval(() => {
-    if(googleMeetCall){
+    if (googleMeetCall) {
       clearInterval(x);
     }
   }, 1000);
-  
- 
 })();
